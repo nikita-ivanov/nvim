4fc9c58169b984bec99982ff11bb3df72ae87d2e
Nikita Ivanov <n.ivanov@ectp.com>
2026-04-10 11:02:03 +0200

    added new colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    opts = { variant = "dawn" },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    opts = { theme = "lotus" },
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    lazy = true,
+    dependencies = { "rktjmp/lush.nvim" },
+  },
+  {
+    "ishan9299/nvim-solarized-lua",
+    lazy = true,
+  },
+}
